9f6c051e16eac1c82de24f456e4d737487c338c6
SebasTamayoA <sebastamayo37@gmail.com>
2022-09-25 16:25:55 -0500

    Modificación de enlaces con thymeleaf

--- src/main/java/com/co/menus/HomeController.java
+++ src/main/java/com/co/menus/HomeController.java
@@ -10,9 +10,26 @@ public class HomeController {
     public String index(){
         return "index";
     }
-
     @GetMapping("/login")
     public String login(){
         return "login";
     }
+    @GetMapping("/blog")
+    public String blog(){ return "blog";}
+    @GetMapping("/calculoimc")
+    public String calculoimc(){ return "calculoimc";}
+    @GetMapping("/contacto")
+    public String contacto(){ return "contacto";}
+    @GetMapping("/politicas")
+    public String politicas(){ return "politicas";}
+    @GetMapping("/recetasbajarpeso")
+    public String recetasbajarpeso(){ return "recetasbajarpeso";}
+    @GetMapping("/recetassubirpeso")
+    public String recetassubirpeso(){ return "recetassubirpeso";}
+    @GetMapping("/registro")
+    public String registro(){ return "registro";}
+    @GetMapping("/sobrenosotros")
+    public String sobrenosotros(){ return "sobrenosotros";}
+
+
 }
